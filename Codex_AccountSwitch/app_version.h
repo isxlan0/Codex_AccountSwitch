17a2f3b6eb2d730b49600b01ab25e7f1a752bcc1
isxlan0 <124419906+isxlan0@users.noreply.github.com>
2026-03-18 21:14:03 +0800

    更新 v1.3.10 补齐本地化翻译

--- Codex_AccountSwitch/app_version.h
+++ Codex_AccountSwitch/app_version.h
@@ -1,9 +1,9 @@
 #pragma once
 
-// v1.3.9
+// v1.3.10
 #define APP_VERSION_MAJOR 1
 #define APP_VERSION_MINOR 3
-#define APP_VERSION_PATCH 9
+#define APP_VERSION_PATCH 10
 #define APP_VERSION_BUILD 0
 
 #define APP_VERSION_STR_HELPER(x) #x
